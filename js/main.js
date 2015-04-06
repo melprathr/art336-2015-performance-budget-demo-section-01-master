bab29adf1cc1b874724e91cd3ba7a547f3002720
melprathr <melprat1@umbc.edu>
2015-04-05 23:16:59 -0400

    Worked on Exercise

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //init jellyfish.js
 jellyfish.init({
-    icon: 'img/loading.gif', // Image to display when image is loading
+    icon: 'img/loading.svg', // Image to display when image is loading
     offset: 0, // How far below fold to begin loading images
     type: 'img', // Type of content to load ('img' or 'iframe')
     callbackBeforeIcons: function ( wrappers ) {}, // Function to run before icon is loaded
